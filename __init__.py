7bcf34033a5676deace1ebf2692cf12623a2cea7
lanyangyin <2436725966@qq.com>
2024-05-03 05:47:40 +0800

    ing

--- __init__.py
+++ __init__.py
@@ -1,24 +1,24 @@
 # coding=utf-8
-from tool import check_config, qr_encode, update_config, dict2cookieformat
-from login import generate, poll, get_buvid3
+from tool import config_B, qr_encode, dict2cookieformat
+from login import generate, poll
+from special import master
 
 
-def start_login(usernum: int = 0):
+def start_login(uid: int):
     """
     扫码登陆获得cookies
-    :param usernum:
+    :param uid:
     :return:
     """
-    uid = ''
-    cookies = {}
-    try:
-        config = check_config()
-        uid = list(config.keys())[usernum]
-        cookies = list(config.values())[usernum]
-    except:
+    configb = config_B(uid=uid, dirname="Biliconfig")
+    cookies = configb.check()
+    islogin = master(cookies).interface_nav()["isLogin"]
+    if islogin:
+        return dict2cookieformat(cookies)
+    else:
         url8qrcode_key = generate()
         url = url8qrcode_key['url']
-        print(qr_encode(url))
+        print(qr_encode(url)["str"])
         qrcode_key = url8qrcode_key['qrcode_key']
         code = poll(qrcode_key)['code']
         print(code)
@@ -29,16 +29,15 @@ def start_login(usernum: int = 0):
             if code_ != code:
                 print(code)
             if code == 0 or code == 86038:
+                cookies = poll_['cookies']
                 break
-        try:
-            cookies = poll_['cookies']
-            uid = cookies['DedeUserID']
-        except:
-            pass
-    cookies.update(get_buvid3()['cookies'])
-    if uid != '':
-        update_config(uid, cookies)
+    if uid == cookies['DedeUserID']:
+        configb.update(cookies)
+    else:
+        uid = int(cookies['DedeUserID'])
+        configb = config_B(uid=uid, dirname="Biliconfig")
+        configb.update(cookies)
     return dict2cookieformat(cookies)
 
 
-print(start_login())
+print(start_login(0))
